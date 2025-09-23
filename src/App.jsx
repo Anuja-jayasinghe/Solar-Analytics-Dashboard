645b1a17e024d219e13ddbb4b0bcca06eea7ee71
Anuja Jayasinghe <anujajayasinhe@gmail.com>
2025-09-23 14:29:28 +0530

    theme setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "./components/ThemeContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
@@ -7,20 +8,22 @@ import "./index.css";
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Sidebar />
-        <div className="main-content">
-          <Navbar />
-          <div className="page-container">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
+    <ThemeProvider>
+      <Router>
+        <div className="app-container">
+          <Sidebar />
+          <div className="main-content">
+            <Navbar />
+            <div className="page-container">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
